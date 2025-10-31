7d77ca865d9e5ee51dfd45c12ce2d3a4f5712705
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-01 00:52:12 +0600

    send menu data

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.jworznu.mongodb.net/?appName=Cluster0";
+// DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jworznu.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +25,14 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const menuCollection = client.db("bistroDB").collection("menu");
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
