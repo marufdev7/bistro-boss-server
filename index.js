87f36ac30a926518cd3b67ac0fbad8313ec21981
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-02 23:10:20 +0600

    reviews data load

--- index.js
+++ index.js
@@ -27,11 +27,17 @@ async function run() {
         await client.connect();
 
         const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewCollection = client.db("bistroDB").collection("reviews");
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
-        })
+        });
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
